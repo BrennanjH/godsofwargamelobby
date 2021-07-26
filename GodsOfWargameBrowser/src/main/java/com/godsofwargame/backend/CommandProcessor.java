1afd1e942bed8ff5f111036d5e49d82f1cd74d3a
BrennanjH <brennanjhammond@gmail.com>
2021-07-26 16:14:13 -0500

    Re-added lost file

---
+++ GodsOfWargameBrowser/src/main/java/com/godsofwargame/backend/CommandProcessor.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.godsofwargame.backend;
+import java.util.HashMap;
+/**
+ *
+ * @author brenn
+ */
+
+public class CommandProcessor{ 
+    public void processor (commandInterface command, GodsofWargame gameState, String Id){
+        command.execute(gameState , Id);
+    }
+}
